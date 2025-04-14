5d7c55a4d185cf155b6a922d507f578a2899ce35
Xelor-DEV <BladeUAOficial@gmail.com>
2025-04-14 12:08:31 -0500

    Test Connection and Game Settings implemented

---
+++ Assets/ProjectAssets/Scripts/Managers/MasterManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Singletons/MasterManager")]
+public class MasterManager : SingletonScriptableObject<MasterManager>
+{
+    [SerializeField] private GameSettings _gameSettings;
+    public static GameSettings GameSettings
+    {
+        get
+        {
+            return Instance._gameSettings;
+        }
+    }
+}
---
+++ Assets/ProjectAssets/Scripts/TestConnect.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        print("Connecting to server.");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Conected to server.");
+        print(PhotonNetwork.LocalPlayer.NickName);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from server for reason " + cause.ToString());
+    }
+}
